3351c62b544ca8a73eb5b013c52597c222478c4a
Terraform User <builder-admin@example.com>
2026-03-09 18:08:07 +0000

    Managed by Terraform

---
+++ main.tf
@@ -0,0 +1,13 @@
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.3"
+  account_id = var.account_id
+  delegate_token = var.delegate_token
+  delegate_name = "terraform-delegate"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:26.01.88303"
+  replicas = 1
+  upgrader_enabled = true
+}
